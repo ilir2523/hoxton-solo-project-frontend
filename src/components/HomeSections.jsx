2ad6fd316f8ae7918c5422af74b49edde05d8149
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-04-04 18:01:53 +0200

    adding data in home

--- src/components/HomeSections.jsx
+++ src/components/HomeSections.jsx
@@ -1,21 +1,65 @@
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { useStore } from '../Store'
+import { numberWithCommas } from '../functions/Functions'
 
 export default function HomeSections() {
 
-    return (
-        <div className="home-sections">
-            <section>
-                <h2>My Accounts</h2>
-                <div className="accounts">
-                    <div><Link to="account">Current & Savings</Link></div>
-                    <div><Link to="account">Term Deposits</Link></div>
-                    <div><Link to="account">Loans and Finances</Link></div>
-                    <div><Link to="account">Credit Card</Link></div>
-                </div>
-            </section>
-            <section><h2>Payment</h2></section>
-            <section><h2>Recent Activity</h2> </section>
-            <section><h2>Quick Access</h2></section>
-        </div>
-    )
+    const user = useStore(store => store.user)
+
+    if (!user) return <h1>Loading...</h1>
+
+    if (user) {
+
+        const savingsAccount = user?.accounts.find(account => account.type === "Savings")
+        // console.log(savingsAccount)
+
+        return (
+            <div className="home-sections">
+                <section>
+                    <h2>My Accounts</h2>
+                    <div className="accounts">
+                        <div><Link to="#">{"Current & Savings: "}{savingsAccount?.currency} {savingsAccount ? numberWithCommas(savingsAccount?.amountInAccount) : null}</Link></div>
+                        <div><Link to="#">Term Deposits</Link></div>
+                        <div><Link to="#">Loans and Finances</Link></div>
+                        <div><Link to="#">Credit Card</Link></div>
+                    </div>
+                </section>
+                <section>
+                    <h2>Payment</h2>
+                    <div className="payments-container">
+                        <div className="payment">
+                            <img src="../../images/transfer-money.png" alt="" />
+                            <span className="payment-name">Transfer money</span>
+                        </div>
+                        <div className="payment">
+                            <img src="../../images/transfer-money.png" alt="" />
+                            <span className="payment-name">Transfer money</span>
+                        </div>
+                        <div className="payment">
+                            <img src="../../images/transfer-money.png" alt="" />
+                            <span className="payment-name">Transfer money</span>
+                        </div>
+                        <div className="payment">
+                            <img src="../../images/transfer-money.png" alt="" />
+                            <span className="payment-name">Transfer money</span>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <h2>Recent Activity</h2>
+                    <div className="activity-container">
+                        {savingsAccount?.transactions?.map(transaction => (
+                            <div className="activity" key={transaction.id}>
+                                <span>{transaction.completedAt.substring(0, 10)}</span>
+                                <span>{savingsAccount?.title}</span>
+                                <span>{transaction ? numberWithCommas(transaction?.amount) : null}{transaction.isPositive ? '(+)' : '(-)'}</span>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+                <section><h2>Quick Access</h2></section>
+            </div>
+        )
+    }
 }
--- src/functions/Functions.jsx
+++ src/functions/Functions.jsx
@@ -1,37 +1,3 @@
-export function logIn(email, password, setUser) {
-    fetch('http://localhost:4001/sign-in', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-    })
-        .then(resp => resp.json())
-        .then(data => {
-            if (data.error) {
-                alert(data.error)
-            } else {
-                localStorage.token = data.token
-                setUser(data.user) // data === { user, token }
-            }
-        })
-}
-
-export function signUp(name, email, phone, address, dateOfBirth, password, setUser) {
-    fetch('http://localhost:4001/sign-up', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({email, password, name, phone, address, dateOfBirth})
-    })
-        .then(resp => resp.json())
-        .then(data => {
-            if (data.error) {
-                alert(data.error)
-            } else {
-                localStorage.token = data.token
-                setUser(data.user) // data === { user, token }
-            }
-        })
+export function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
